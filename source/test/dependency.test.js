4769d60473c2725521c9069645978995775a9777
virtualpatterns.com <code@virtualpatterns.com>
2022-03-16 21:12:23 -0400

    post-update

--- source/test/dependency.test.js
+++ source/test/dependency.test.js
@@ -8,6 +8,7 @@ Test('default', async (test) => {
 
   // test.log(dependency)
   test.deepEqual(dependency.missing, {})
+  test.deepEqual(dependency.section, {})
   test.deepEqual(dependency.unused, [])
 
 })
